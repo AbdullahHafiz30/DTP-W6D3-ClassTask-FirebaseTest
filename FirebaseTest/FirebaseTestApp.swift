25fdf4d3e93fe83e7e5f083718391876eefc2f4e
ABDULLAH HAFIZ <ABDULLAH_MH30@OUTLOOK.COM>
2025-03-25 11:14:35 +0300

    Initial Commit

---
+++ FirebaseTest/FirebaseTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseTestApp.swift
+//  FirebaseTest
+//
+//  Created by Abdullah Hafiz on 25/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
